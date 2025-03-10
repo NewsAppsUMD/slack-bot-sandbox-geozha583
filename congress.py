bd7ca8311aefb033970531bfd2c77eadf8ea5b65
Geoffrey Zhang <50123131+geozha583@users.noreply.github.com>
2025-03-10 20:52:32 +0000

    added congress stuff

---
+++ congress.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import os
+
+congress_key = os.environ.get('CONGRESS_API_KEY')
+
+url = f"https://api.congress.gov/v3/committee-report/119/hrpt?api_key={congress_key}&format=json"
+
+r = requests.get(url)
+
+results = r.json()
+
+
+
+first_report = results["reports"][0]['url']
+
+fr = requests.get(first_report + f"&api_key={congress_key}")
+
+result = fr.json()
+
+print(result)
+
+for i in range(len(results["reports"])):
+    if results["reports"][i]["number"] > 5:
+        print(["reports"][i]) #error here!
